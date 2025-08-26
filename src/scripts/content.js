d581a4bf24c1484df3d2d1dbfbd36262f8ff1788
jagobainda <jagobainda@gmail.com>
2025-08-27 01:21:18 +0200

    Fetching info from cswatch api done

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -4,9 +4,11 @@
     const template = createTemplate();
 
     const profileCustomizationArea = document.querySelector('.profile_customization_area');
+    const profileLeftcol = document.querySelector('.profile_leftcol');
 
     setTimeout(() => {
         if (profileCustomizationArea) profileCustomizationArea.insertBefore(template, profileCustomizationArea.firstChild);
+        if (!profileCustomizationArea && profileLeftcol) profileLeftcol.insertBefore(template, profileLeftcol.firstChild);
 
         setEventListeners();
         loadUserStats();
@@ -16,24 +18,78 @@
 function setEventListeners() {
     const copyToClipboardDiv = document.getElementById('copy-cs-watch-stats-to-clipboard');
     copyToClipboardDiv?.addEventListener('click', () => copyStatsToClipboard());
+
+
 }
 
 async function loadUserStats() {
     const userId = getUserId();
 
-    console.log(userId);
+    if (!userId) {
+        showError('User ID not found');
+        return;
+    }
 
-    // TODO: Load user stats from API
+    await chrome.runtime.sendMessage(
+        { action: 'fetchPlayerData', userId },
+        (response) => {
+            if (!response || !response.success) {
+                showError('Failed to fetch player data');
+                return;
+            }
+
+            const isAutoflagged = response.data.player.autoflagger.length > 0;
+
+            if (isAutoflagged) showAutoflagged();
+
+            displayStats(response.data.csWatchAnalysis.breakdown, response.data.csWatchAnalysis.totalScore)
+        }
+    );
 }
 
 function getUserId() {
-    const url = window.location.href;
+    const steamIdSpan = document.getElementById('steam-id-hidden');
 
-    if (url.includes("/id/")) return url.split("/id/")[1]?.split("/")[0];
+    if (!steamIdSpan) return null;
 
-    if (url.includes("/profiles/")) return url.split("/profiles/")[1]?.split("/")[0];
+    return steamIdSpan.innerText;
+}
 
-    return null;
+function showAutoflagged() {
+    const container = document.getElementById('is-autoflagged');
+
+    container.style.display = 'block';
+}
+
+function displayStats(data, totalScore) {
+    const kdRatio = document.getElementById('cs-watch-stats-kd-ratio');
+    const timeToDamage = document.getElementById('cs-watch-stats-time-to-damage');
+    const preaimDegree = document.getElementById('cs-watch-stats-preaim-degree');
+    const aimRating = document.getElementById('cs-watch-stats-aim-rating');
+    const winRate = document.getElementById('cs-watch-stats-win-rate');
+    const riskRate = document.getElementById('cs-watch-stats-risk-rate');
+
+    if (!kdRatio || !timeToDamage || !preaimDegree || !aimRating || !winRate || !riskRate) return
+
+    kdRatio.innerText = data.find(item => item.type === 'kd').value;
+    timeToDamage.innerText = data.find(item => item.type === 'reaction').value + " ms";
+    preaimDegree.innerText = data.find(item => item.type === 'preaim').value + "º";
+    aimRating.innerText = data.find(item => item.type === 'aim').value + " %";
+    winRate.innerText = data.find(item => item.type === 'winrate').value + " %";
+    riskRate.innerText = totalScore + " %";
+}
+
+function showError(message) {
+    const container = document.getElementById('cs-watch-stats-container');
+
+    if (!container) return;
+
+    const error = document.createElement('p');
+
+    error.innerText = message;
+
+    container.innerHTML = '';
+    container.appendChild(error);
 }
 
 async function copyStatsToClipboard() {
@@ -82,6 +138,11 @@ function createTemplate() {
     csWatchLinkTitle.innerText = 'CSWATCH.IN';
     csWatchLinkTitle.className = 'cs-watch-link-title';
 
+    const isAutoflagged = document.createElement('div');
+    isAutoflagged.innerText = 'Autoflagged';
+    isAutoflagged.className = 'is-autoflagged';
+    isAutoflagged.id = 'is-autoflagged';
+
     const copyToClipboard = document.createElement('div');
     copyToClipboard.innerText = 'Copy stats to clipboard';
     copyToClipboard.className = 'copy-cs-watch-stats-to-clipboard';
@@ -92,12 +153,13 @@ function createTemplate() {
 
     const favoriteGroupContent = document.createElement('div');
     favoriteGroupContent.className = 'showcase_content_bg cs-watch-stats-container';
+    favoriteGroupContent.id = 'cs-watch-stats-container';
 
     const kdRatioContainer = document.createElement('div');
     kdRatioContainer.className = 'cs-watch-stats-item';
 
     const kdRatio = document.createElement('span');
-    kdRatio.innerText = '1.10';
+    kdRatio.innerText = '0.00';
     kdRatio.id = 'cs-watch-stats-kd-ratio';
     kdRatio.className = 'cs-watch-stats-item-value';
 
@@ -109,7 +171,7 @@ function createTemplate() {
     timeToDamageContainer.className = 'cs-watch-stats-item';
 
     const timeToDamage = document.createElement('span');
-    timeToDamage.innerText = '646.6 ms';
+    timeToDamage.innerText = '000.0 ms';
     timeToDamage.id = 'cs-watch-stats-time-to-damage';
     timeToDamage.className = 'cs-watch-stats-item-value';
 
@@ -121,7 +183,7 @@ function createTemplate() {
     preaimDegreeContainer.className = 'cs-watch-stats-item';
 
     const preaimDegree = document.createElement('span');
-    preaimDegree.innerText = '10.9º';
+    preaimDegree.innerText = '0.0º';
     preaimDegree.id = 'cs-watch-stats-preaim-degree';
     preaimDegree.className = 'cs-watch-stats-item-value';
 
@@ -133,7 +195,7 @@ function createTemplate() {
     aimRatingContainer.className = 'cs-watch-stats-item';
 
     const aimRating = document.createElement('span');
-    aimRating.innerText = '75.2 %';
+    aimRating.innerText = '000.0 %';
     aimRating.id = 'cs-watch-stats-aim-rating';
     aimRating.className = 'cs-watch-stats-item-value';
 
@@ -145,7 +207,7 @@ function createTemplate() {
     winRateContainer.className = 'cs-watch-stats-item';
 
     const winRate = document.createElement('span');
-    winRate.innerText = '56.7 %';
+    winRate.innerText = '000.0 %';
     winRate.id = 'cs-watch-stats-win-rate';
     winRate.className = 'cs-watch-stats-item-value';
 
@@ -171,6 +233,7 @@ function createTemplate() {
     headerContent.appendChild(headerTitle);
     headerTitle.appendChild(csWatchLogo);
     headerTitle.appendChild(csWatchLinkTitle);
+    headerTitle.appendChild(isAutoflagged);
     headerContent.appendChild(copyToClipboard);
 
     profileCustomization.appendChild(favoriteGroup);
